0e408e003de851e22d8e8020b3e65c989070e0dd
Leandro Cavalcanti <leand@DESKTOP-LFIP8ID.(none)>
2020-12-18 16:20:56 -0300

    Carrinhp e produto

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using ObjetoArgumento.classes;
+
+namespace ObjetoArgumento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ops1 = 0, quant = 0;
+            Carrinho carrinho = new Carrinho();
+
+            Console.WriteLine($"--------------------\nAdicionando produtos\n--------------------");
+            do
+            {   
+                Console.WriteLine("[1] Adicionar produtos\n[2] Remover produtos\n[3] Ver lista de produtos\n[4] Sair");
+                ops1 = int.Parse(Console.ReadLine());
+
+                while (ops1 != 1 && ops1 != 2 && ops1 != 3 && ops1 != 4)
+                {
+                    Console.WriteLine($"Opção inválida.\n[1] Adicionar produtos\n[2] Remover produtos\n[3] Ver lista de produtos\n[4] Sair");
+                    ops1 = int.Parse(Console.ReadLine());
+                }
+
+                switch (ops1)
+                {
+                    case 1:
+                        Console.Write($"Digite quantos produtos você deseja adicionar: ");
+                        quant = int.Parse(Console.ReadLine());
+
+                        while (quant <= 0)
+                        {
+                            Console.Write($"Numero inválido.\nDigite novamente: ");
+                            quant = int.Parse(Console.ReadLine());
+                        }
+
+                        for (var i = 0; i < quant; i++)
+                        {
+                            Produto produto = new Produto();
+                            Console.Write($"Digite o codigo do produto: ");
+                            produto.Codigo = int.Parse(Console.ReadLine());
+                            Console.Write($"Digite o nome do produto: ");
+                            produto.Nome = Console.ReadLine();
+                            Console.Write($"Digite o preço do produto: R$ ");
+                            produto.Preco = float.Parse(Console.ReadLine());
+                            carrinho.AdicionarProduto(produto);
+                        }
+                    break;
+
+                    case 2:
+                        Console.Write($"Digite o código do produto que você deseja remover: ");
+                        Produto excluir = new Produto();
+                        excluir.Codigo = int.Parse(Console.ReadLine());
+                        carrinho.RemoverProduto(excluir.Codigo);
+                    break;
+
+                    case 3:
+                        carrinho.MostrarProduto();
+                    break;
+                }
+            } while (ops1 != 4);
+
+            //carrinho.RemoverProduto(produto1);
+
+            //Produto produtoNovo = new Produto();
+            //produtoNovo.Nome = "Fone com Microfone Chroma 7.1";
+            //produtoNovo.Preco = 600;
+
+            //carrinho.AlterarItem(2, produtoNovo);
+            //carrinho.MostrarProduto();
+        }
+    }
+}
---
+++ classes/Produto.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ObjetoArgumento.classes
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        
+        public string Nome { get; set; }
+        
+        public float Preco { get; set; }
+
+        public Produto(int _codigo, string _nome, float _preco){
+            this.Codigo = _codigo;
+            this.Nome = _nome;
+            this.Preco = _preco;
+        }
+
+        public Produto()
+        {
+        }
+    }
+}
